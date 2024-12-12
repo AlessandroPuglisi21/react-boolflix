55fc27141268e5cc56e642fad70059beed6c7bbf
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-12-12 13:15:38 +0100

    ADD:SearchBar Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import GlobalContext from "../context/GlobalContext"
 import axios from "axios"
+import HomePage from "./pages/HomePage"
 
 function App() {
 
 
   return (
     <>
+    <HomePage></HomePage>
     </>
   )
 }
