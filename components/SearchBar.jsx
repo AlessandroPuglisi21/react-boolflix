e8b51ca909663e806b5c348d000d63af6857e57f
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-12-12 16:02:00 +0100

    ADD: TvSeries

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 
-export default function SearchBar({film,setFilm,onSearch}) {
+export default function SearchBar({film,setFilm,tvSerie, setTvSerie, onSearch}) {
 const handleInput = (e) => {
     setFilm (e.target.value)
+    setTvSerie (e.target.value)
 }
 
     return(
     <div>
         <input type="text" 
-        placeholder="Cerca un film..."
-        value={film}
+        placeholder="Cerca un film o una serie TV"
+        value={film || tvSerie}
         onChange={handleInput}
         />
         <button onClick={onSearch}>Cerca</button>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,24 +5,29 @@ import Flags from "../../context/Flags";
 
 export default function HomePage() {
   const [film, setFilm] = useState("");
+  const [tvSerie, setTvSerie] = useState("")
   const [results, setResults] = useState([]);
-
+  
+  
   const handleSearch = () => {
     if (!film) return;
+  
+    Promise.all([
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=da445b1f8c881fe7432f8928e0e4f76f&query=${film}`),
+      axios.get(`https://api.themoviedb.org/3/search/tv?api_key=da445b1f8c881fe7432f8928e0e4f76f&language=it_IT&query=${tvSerie}`)
+    ])
+      .then(([res1, res2]) => {
 
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=da445b1f8c881fe7432f8928e0e4f76f&query=${film}`
-      )
-      .then((res) => {
-        setResults(res.data.results);
-        console.log(res.data.results);
+        const allResults = [...res1.data.results, ...res2.data.results]
+        setResults(allResults); 
+        console.log(allResults)
       })
       .catch((err) => {
-        console.log("Errore durante la ricerca", err);
+        console.log("Errore durante le chiamate", err);
         setResults([]);
       });
   };
+  
 
   function flags(language) {
     if (language === "en") {
@@ -36,16 +41,16 @@ export default function HomePage() {
 
   return (
     <>
-      <SearchBar film={film} setFilm={setFilm} onSearch={handleSearch} />
-      <h1>Film Trovati</h1>
+      <SearchBar film={film} setFilm={setFilm} setTvSerie = {setTvSerie} onSearch={handleSearch} />
+      <h1>Risultati Trovati</h1>
       <ul>
         {results.length > 0 ? (
           results.map((result) => {
             const language = result.original_language
             return(
             <li key={result.id}>
-              <h1>Titolo: {result.title} </h1>
-              <h3>Titolo Originale: {result.original_title}</h3>
+              <h1>Titolo: {result.title} {result.name} </h1>
+              <h3>Titolo Originale: {result.original_title} {result.original_name}</h3>
               <p>Lingua:{result.original_language}<img src={flags(language)} alt="" style={{width:"35px"}}/> </p>
               <p>Voto: {result.vote_average.toFixed(1)}</p>
             </li>
