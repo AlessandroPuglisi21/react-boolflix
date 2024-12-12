8f57f0fdff8857f05f89f7476764e6af3548709e
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-12-12 15:13:23 +0100

    ADD: Flags

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,48 +1,56 @@
 import SearchBar from "../../components/SearchBar";
 import { useState } from "react";
 import axios from "axios";
+import Flags from "../../context/Flags";
 
 export default function HomePage() {
   const [film, setFilm] = useState("");
   const [results, setResults] = useState([]);
-  
 
   const handleSearch = () => {
-    if (!film) return; 
+    if (!film) return;
 
     axios
       .get(
         `https://api.themoviedb.org/3/search/movie?api_key=da445b1f8c881fe7432f8928e0e4f76f&query=${film}`
       )
       .then((res) => {
-        setResults(res.data.results); 
-        console.log(res.data.results); 
+        setResults(res.data.results);
+        console.log(res.data.results);
       })
       .catch((err) => {
         console.log("Errore durante la ricerca", err);
-        setResults([]); 
+        setResults([]);
       });
   };
 
+  function flags(language) {
+    if (language === "en") {
+      return Flags.usa;
+    } else if (language === "it"){
+        return Flags.ita
+    } else if (language === 'ja'){
+        return Flags.jap
+    }
+  }
+
   return (
     <>
+      <SearchBar film={film} setFilm={setFilm} onSearch={handleSearch} />
       <h1>Film Trovati</h1>
-      <SearchBar
-        film={film}
-        setFilm={setFilm}
-        onSearch={handleSearch}
-      />
       <ul>
         {results.length > 0 ? (
-          results.map((result) => (
+          results.map((result) => {
+            const language = result.original_language
+            return(
             <li key={result.id}>
-              Titolo: {result.title} <br />
-              Titolo Originale: {result.original_title} <br />
-              Lingua: {result.original_language} <br />
-              Voto: {result.vote_average.toFixed(1)}
-
+              <h1>Titolo: {result.title} </h1>
+              <h3>Titolo Originale: {result.original_title}</h3>
+              <p>Lingua:{result.original_language}<img src={flags(language)} alt="" style={{width:"35px"}}/> </p>
+              <p>Voto: {result.vote_average.toFixed(1)}</p>
             </li>
-          ))
+            )
+          })
         ) : (
           <p>Nessun risultato trovato.</p>
         )}
